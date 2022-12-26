c5b4c9dc4655db7fcf776b453bf2bc30c040849a
Will <will@onezerohosting.com>
2022-12-26 12:24:15 -0800

    update dev script for rollup deps

---
+++ utils/watch-rollup.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const { rollup } = require('rollup');
+const chokidar = require('chokidar');
+
+const watchDir = 'dist';
+const deps = [
+    {
+        input: 'node_modules/uuid/dist/esm-browser/v5.js',
+        watch: `${watchDir}/uuidv5.js`,
+        name: 'uuidv5'
+    }
+]
+const watchFiles = deps.map(dep => dep.watch);
+
+console.log({watchFiles});
+
+const watcher = chokidar.watch(watchFiles, {
+    persistent: true
+});
+
+watcher.on('ready', async () => {
+    build();
+});
+
+watcher.on('unlink', async () => {
+    build();
+});
+
+async function build() {
+    watchFiles.map(async (path) => {
+        if (!fs.existsSync(path)) {
+            const dep = deps.find((dep) => dep.watch === path);
+
+            console.log(`rolling up ${path}...`);
+            
+            try {
+                const bundle = await rollup({
+                    input: dep.input,
+                });
+                const { output } = await bundle.generate({
+                    format: 'iife',
+                    file: dep.watch,
+                    name: dep.name,
+                });
+                fs.writeFileSync(dep.watch, output[0].code);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    });
+}
