7a1f73a72e01b5bda65e5d68d98418bbb788f5b2
Will <will@onezerohosting.com>
2023-01-03 19:38:41 -0800

    .

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -92,41 +92,41 @@ module.exports = (async () => {
                 });
             }
         });
-    });
-    test('popup page - that only ONE tab is ever opened', async ({ page, context, serviceWorker }) => {
-        // loop size of 100 should take about 1 second each
-        test.setTimeout(60000 * 2);
-
-        const re = new RegExp(`devtools:\/\/.*ws=localhost:${ports[0]}.*`)
-        const inputs = {
-            port: await page.locator(ids.inputs.port),
-            host: await page.locator(ids.inputs.host)
-        }
-        try {
-            let successes = 0;
-
-            await page.goto(`chrome-extension://${serviceWorker.url().split('/')[2]}/dist/index.html`);
-            // this timeout is for the default 9229 tab to open and settle otherwise it will just popup and grab focus since those are the defaults
-            await new Promise(resolve => setTimeout(resolve, 15000));
-            await page.bringToFront();
-            await inputs.port.clear();
-            await inputs.port.type(`${ports[0]}`);
-            await inputs.host.press('Enter');
-            for (let loop in Object.keys(Array.from(new Array(100)))) {
-                await context.waitForEvent('page');
-                const pages = context.pages().filter(page => page.url().match(re));
-                expect(pages.length).toBe(1);
-                await pages[0].close();
-                successes += 1;
+        test('popup page - that only ONE tab is ever opened', async ({ page, context, serviceWorker }) => {
+            // loop size of 100 should take about 1 second each
+            test.setTimeout(60000 * 2);
+    
+            const re = new RegExp(`devtools:\/\/.*ws=localhost:${ports[0]}.*`)
+            const inputs = {
+                port: await page.locator(ids.inputs.port),
+                host: await page.locator(ids.inputs.host)
             }
-            expect(successes).toBe(100);
-        } finally {
-            await serviceWorker.evaluate(async () => {
-                await Promise.all([
-                    chrome.storage.local.clear(),
-                    chrome.storage.session.clear()
-                ]);
-            });
-        }
-    }, );
+            try {
+                let successes = 0;
+    
+                await page.goto(`chrome-extension://${serviceWorker.url().split('/')[2]}/dist/index.html`);
+                // this timeout is for the default 9229 tab to open and settle otherwise it will just popup and grab focus since those are the defaults
+                await new Promise(resolve => setTimeout(resolve, 3000));
+                await page.bringToFront();
+                await inputs.port.clear();
+                await inputs.port.type(`${ports[0]}`);
+                await inputs.host.press('Enter');
+                for (let loop in Object.keys(Array.from(new Array(100)))) {
+                    await context.waitForEvent('page');
+                    const pages = context.pages().filter(page => page.url().match(re));
+                    expect(pages.length).toBe(1);
+                    await pages[0].close();
+                    successes += 1;
+                }
+                expect(successes).toBe(100);
+            } finally {
+                await serviceWorker.evaluate(async () => {
+                    await Promise.all([
+                        chrome.storage.local.clear(),
+                        chrome.storage.session.clear()
+                    ]);
+                });
+            }
+        }, );
+    });
 })();
