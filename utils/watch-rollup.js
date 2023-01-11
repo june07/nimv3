1a27e319204a9662934a64ed27a4e096d793a8f5
Will <will@onezerohosting.com>
2023-01-11 09:02:11 -0800

    update rollup to copy other deps

--- utils/watch-rollup.js
+++ utils/watch-rollup.js
@@ -8,11 +8,31 @@ const deps = [
         input: 'node_modules/uuid/dist/esm-browser/v5.js',
         watch: `${watchDir}/uuidv5.min.js`,
         name: 'uuidv5'
+    },
+    {
+        input: 'node_modules/tweetnacl/nacl-fast.min.js',
+        watch: `${watchDir}/nacl-fast.min.js`,
+    },
+    {
+        input: 'node_modules/tweetnacl-util/nacl-util.min.js',
+        watch: `${watchDir}/nacl-util.min.js`,
+    },
+    {
+        input: 'node_modules/amplitude-js/amplitude.umd.min.js',
+        watch: `${watchDir}/amplitude.umd.min.js`,
+    },
+    {
+        input: 'node_modules/async/dist/async.min.js',
+        watch: `${watchDir}/async.min.js`,
+    },
+    {
+        input: 'node_modules/socket.io-client/dist/socket.io.min.js',
+        watch: `${watchDir}/socket.io.min.js`,
     }
 ]
 const watchFiles = deps.map(dep => dep.watch);
 
-console.log({watchFiles});
+console.log({ watchFiles });
 
 const watcher = chokidar.watch(watchFiles, {
     persistent: true
@@ -31,23 +51,27 @@ async function build() {
         if (!fs.existsSync(path)) {
             const dep = deps.find((dep) => dep.watch === path);
 
-            console.log(`rolling up ${path}...`);
-            
-            try {
-                const bundle = await rollup({
-                    input: dep.input,
-                });
-                const { output } = await bundle.generate({
-                    compact: true,
-                    format: 'iife',
-                    file: dep.watch,
-                    name: dep.name,
-                });
-                fs.writeFileSync(dep.watch, output[0].code);
-            } catch (error) {
-                console.error(error);
+            if (dep.input.match(/nacl-fast|nacl-util|amplitude|async|socket.io/)) {
+                console.log(`copying dep ${dep.input} to ${dep.watch}`);
+                fs.copyFileSync(dep.input, dep.watch);
+            } else {
+                console.log(`rolling up ${path}...`);
+                try {
+                    const bundle = await rollup({
+                        input: dep.input,
+                    });
+                    const { output } = await bundle.generate({
+                        compact: true,
+                        format: 'iife',
+                        file: dep.watch,
+                        name: dep.name,
+                    });
+                    fs.writeFileSync(dep.watch, output[0].code);
+                } catch (error) {
+                    console.error(error);
+                }
             }
         }
     });
-    
+
 }
