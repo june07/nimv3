f89acc9cf0c04a640da7385cce1c686552a2ca0a
Will <will@onezerohosting.com>
2023-01-14 13:03:40 -0800

    update vuetify, add labs component

--- src/ui/plugins/vuetify.js
+++ src/ui/plugins/vuetify.js
@@ -9,9 +9,13 @@
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { VVirtualScroll } from 'vuetify/labs/VVirtualScroll'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  components: {
+    VVirtualScroll
+  },
   theme: {
     themes: {
       light: {
