37f655d5c707e7bdd13c920778c95c318b3f1c57
Will <will@onezerohosting.com>
2023-01-14 14:03:16 -0800

    add close button to overlay

--- src/ui/components/NiMessagesOverlay.vue
+++ src/ui/components/NiMessagesOverlay.vue
@@ -1,12 +1,12 @@
 <template>
     <v-overlay class="d-flex justify-center align-center">
-        <v-card color="rgba(255,255,255,0.95)" width="790" height="590" class="pt-4 px-4">
+        <v-card color="rgba(255,255,255,0.95)" width="790" class="pt-4 px-4">
             <v-card-title class="text-uppercase font-weight-bold" :class="theme === 'dark' ? 'text-black' : ''">{{ i18nString('optionsNotifications') }}</v-card-title>
             <v-card-text class="text-body-1" :class="theme === 'dark' ? 'text-black' : ''">
                 <span v-if="props.messages?.length">You have {{ unread?.length }} unread notifications.</span>
                 <span v-else>{{ i18nString('noNotifications') }}</span>
             </v-card-text>
-            <v-virtual-scroll height="400" :items="messages">
+            <v-virtual-scroll height="440" :items="messages">
                 <template v-slot:default="{ item }">
                     <v-list-item :key="item.received" :title="item.title" :subtitle="item.subtitle">
                         <template v-slot:prepend>
@@ -26,6 +26,9 @@
                     </v-list-item>
                 </template>
             </v-virtual-scroll>
+            <v-card-actions class="d-flex justify-end pb-0">
+                <v-btn variant="plain" density="compact" @click="$emit('close')" :class="theme === 'dark' ? 'text-black' : ''">close</v-btn>
+            </v-card-actions>
         </v-card>
     </v-overlay>
 </template>
