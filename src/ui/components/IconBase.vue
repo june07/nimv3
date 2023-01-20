7422cbf5c6112829eb846cf70b9a1137b8c8e1cf
Will <will@onezerohosting.com>
2023-01-20 09:49:24 -0800

    update add icon components, remove standalone svg

---
+++ src/ui/components/IconBase.vue
@@ -0,0 +1,22 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg"
+         :width="width"
+         :height="height"
+         :viewBox="viewBox"
+         :aria-labelledby="iconName"
+         :preserveAspectRatio="preserveAspectRatio">
+        <title :id="iconName" lang="en">{{iconName}}</title>
+        <slot />
+    </svg>
+</template>
+<script>
+export default {
+    props: {
+        viewBox: { type: String, default: "0 0 24 24" },
+        iconName: { type: String, default: "" },
+        width: { type: [Number, String], default: 24 },
+        height: { type: [Number, String], default: 24 },
+        preserveAspectRatio: { type: String, default: "xMidYMid meet" },
+    },
+};
+</script>
--- src/ui/components/ShareMenu.vue
+++ src/ui/components/ShareMenu.vue
@@ -8,9 +8,13 @@
         <v-list class="rounded-xl">
             <v-list-item v-for="type in data.networks" :key="type.network" dense>
                 <ShareNetwork url="https://june07.com/nim" :network="type.network" :title="data.sharing.title" :description="data.sharing.description" :quote="data.sharing.quote" :hashtags="data.sharing.hashtags" :twitterUser="data.sharing.twitterUser">
-                    <v-icon class="mr-2" :color="type.color">
-                        <span class="material-icons small-icon">{{ type.icon }}</span>
+                    <v-icon class="mr-2" :color="type.color" v-if="!type.network.match(/twitter|whatsapp/)">
+                        <span class="material-icons">{{ type.icon }}</span>
                     </v-icon>
+                    <icon-base class="mr-2" v-else :icon-name="type.name">
+                        <icon-twitter v-if="type.network.match(/twitter/)"></icon-twitter>
+                        <icon-whatsapp v-if="type.network.match(/whatsapp/)"></icon-whatsapp>
+                    </icon-base>
                     <span>{{ type.name }}</span>
                 </ShareNetwork>
             </v-list-item>
@@ -23,6 +27,10 @@ a {
 }
 </style>
 <script setup>
+import IconBase from './IconBase';
+import IconTwitter from './IconTwitter';
+import IconWhatsapp from './IconWhatsapp';
+
 const props = defineProps({
     copy: String,
     size: String,
@@ -59,13 +67,13 @@ const data = {
         {
             network: "twitter",
             name: "Twitter",
-            icon: '',
+            icon: "",
             color: "#1da1f2",
         },
         {
             network: "whatsapp",
             name: "Whatsapp",
-            icon: '',
+            icon: "",
             color: "#25d366",
         },
     ],
